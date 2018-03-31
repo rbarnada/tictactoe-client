080dfe2a1ddc5c2c73d2b2a035742875fe1ce457
rbarnada <ryan.barnada@gmail.com>
2018-03-31 18:15:11 -0400

    Built game game logic
    created logic for adding a move and determining a winner/tie

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,5 +7,60 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  // player variables
+  const playerOne = 'X'
+  const playerTwo = 'O'
+  let currentPlayer = playerOne
+  const gameBoard = ['', '', '', '', '', '', '', '', '']
+  // turn variable. odd numbers = p1, even p2.
+  let turn = 1
+  let gameOver = false
+
+  $('.spaces').on('click', function (event) {
+    $(event.target).append(`<h1>${currentPlayer}</h1>`)
+    // Sets X or O to position in array
+    const currentVal = $(event.target).data().value
+    gameBoard[currentVal] = currentPlayer
+
+    console.log(gameBoard)
+
+    // check for winner
+    if ((gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') ||
+        (gameBoard[0] === 'X' && gameBoard[3] === 'X' && gameBoard[6] === 'X') ||
+        (gameBoard[0] === 'X' && gameBoard[4] === 'X' && gameBoard[8] === 'X') ||
+        (gameBoard[2] === 'X' && gameBoard[5] === 'X' && gameBoard[8] === 'X') ||
+        (gameBoard[6] === 'X' && gameBoard[7] === 'X' && gameBoard[8] === 'X') ||
+        (gameBoard[3] === 'X' && gameBoard[4] === 'X' && gameBoard[5] === 'X') ||
+        (gameBoard[1] === 'X' && gameBoard[4] === 'X' && gameBoard[7] === 'X') ||
+        (gameBoard[2] === 'X' && gameBoard[4] === 'X' && gameBoard[6] === 'X')) {
+      console.log('Player 1 wins')
+      $('#winner-message').text('Player 1 wins')
+      $('.spaces').off('click')
+      gameOver = true
+    } else if ((gameBoard[0] === 'O' && gameBoard[1] === 'O' && gameBoard[2] === 'O') ||
+        (gameBoard[0] === 'O' && gameBoard[3] === 'O' && gameBoard[6] === 'O') ||
+        (gameBoard[0] === 'O' && gameBoard[4] === 'O' && gameBoard[8] === 'O') ||
+        (gameBoard[2] === 'O' && gameBoard[5] === 'O' && gameBoard[8] === 'O') ||
+        (gameBoard[6] === 'O' && gameBoard[7] === 'O' && gameBoard[8] === 'O') ||
+        (gameBoard[3] === 'O' && gameBoard[4] === 'O' && gameBoard[5] === 'O') ||
+        (gameBoard[1] === 'O' && gameBoard[4] === 'O' && gameBoard[7] === 'O') ||
+        (gameBoard[2] === 'O' && gameBoard[4] === 'O' && gameBoard[6] === 'O')) {
+      console.log('Player 2 wins')
+      $('#winner-message').text('Player 2 wins')
+      $('.spaces').off('click')
+      gameOver = true
+    } else if (gameBoard[0] !== '' && gameBoard[1] !== '' && gameBoard[2] !== '' && gameBoard[3] !== '' && gameBoard[4] !== '' && gameBoard[5] !== '' && gameBoard[6] !== '' && gameBoard[7] !== '' && gameBoard[8] !== '') {
+      $('#winner-message').text("It's a tie")
+      gameOver = true
+    }
+
+    turn++
+    // console.log(turn)
+    if (turn % 2 === 0) {
+      currentPlayer = playerTwo
+    } else {
+      currentPlayer = playerOne
+    }
+    $(event.target).off('click')
+  })
 })
