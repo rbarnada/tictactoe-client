ce35476c9e62c2a29302f862429c7fdf35771769
rbarnada <ryan.barnada@gmail.com>
2018-04-01 10:41:57 -0400

    Restart Changes
    reset button only appears at end of game

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -15,17 +15,27 @@ $(() => {
   // turn variable. odd numbers = p1, even p2.
   let turn = 1
   let gameOver = false
+  $('#restart').hide()
 
-  $('.spaces').on('click', function (event) {
-    $(event.target).append(`<h1>${currentPlayer}</h1>`)
-    // Sets X or O to position in array
-    const currentVal = $(event.target).data().value
-    gameBoard[currentVal] = currentPlayer
+  const newGame = function () {
+    $('.spaces').on('click', function (event) {
+      $(event.target).append(`<h1>${currentPlayer}</h1>`)
+      // Sets X or O to position in array
+      const currentVal = $(event.target).data().value
+      gameBoard[currentVal] = currentPlayer
+      turn += 1
+      // console.log(turn)
+      if (turn % 2 === 0) {
+        currentPlayer = playerTwo
+      } else {
+        currentPlayer = playerOne
+      }
+      $(event.target).off('click')
+      console.log(turn)
+      console.log(gameBoard)
 
-    console.log(gameBoard)
-
-    // check for winner
-    if ((gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') ||
+      // check for winner
+      if ((gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') ||
         (gameBoard[0] === 'X' && gameBoard[3] === 'X' && gameBoard[6] === 'X') ||
         (gameBoard[0] === 'X' && gameBoard[4] === 'X' && gameBoard[8] === 'X') ||
         (gameBoard[2] === 'X' && gameBoard[5] === 'X' && gameBoard[8] === 'X') ||
@@ -33,11 +43,12 @@ $(() => {
         (gameBoard[3] === 'X' && gameBoard[4] === 'X' && gameBoard[5] === 'X') ||
         (gameBoard[1] === 'X' && gameBoard[4] === 'X' && gameBoard[7] === 'X') ||
         (gameBoard[2] === 'X' && gameBoard[4] === 'X' && gameBoard[6] === 'X')) {
-      console.log('Player 1 wins')
-      $('#winner-message').text('Player 1 wins')
-      $('.spaces').off('click')
-      gameOver = true
-    } else if ((gameBoard[0] === 'O' && gameBoard[1] === 'O' && gameBoard[2] === 'O') ||
+        console.log('Player 1 wins')
+        $('#winner-message').text('Player 1 wins')
+        $('.spaces').off('click')
+        gameOver = true
+        $('#restart').show()
+      } else if ((gameBoard[0] === 'O' && gameBoard[1] === 'O' && gameBoard[2] === 'O') ||
         (gameBoard[0] === 'O' && gameBoard[3] === 'O' && gameBoard[6] === 'O') ||
         (gameBoard[0] === 'O' && gameBoard[4] === 'O' && gameBoard[8] === 'O') ||
         (gameBoard[2] === 'O' && gameBoard[5] === 'O' && gameBoard[8] === 'O') ||
@@ -45,22 +56,41 @@ $(() => {
         (gameBoard[3] === 'O' && gameBoard[4] === 'O' && gameBoard[5] === 'O') ||
         (gameBoard[1] === 'O' && gameBoard[4] === 'O' && gameBoard[7] === 'O') ||
         (gameBoard[2] === 'O' && gameBoard[4] === 'O' && gameBoard[6] === 'O')) {
-      console.log('Player 2 wins')
-      $('#winner-message').text('Player 2 wins')
-      $('.spaces').off('click')
-      gameOver = true
-    } else if (gameBoard[0] !== '' && gameBoard[1] !== '' && gameBoard[2] !== '' && gameBoard[3] !== '' && gameBoard[4] !== '' && gameBoard[5] !== '' && gameBoard[6] !== '' && gameBoard[7] !== '' && gameBoard[8] !== '') {
-      $('#winner-message').text("It's a tie")
-      gameOver = true
-    }
+        console.log('Player 2 wins')
+        $('#winner-message').text('Player 2 wins')
+        $('.spaces').off('click')
+        gameOver = true
+        $('#restart').show()
+      } else if (gameBoard[0] !== '' && gameBoard[1] !== '' && gameBoard[2] !== '' && gameBoard[3] !== '' && gameBoard[4] !== '' && gameBoard[5] !== '' && gameBoard[6] !== '' && gameBoard[7] !== '' && gameBoard[8] !== '') {
+        $('#winner-message').text("It's a tie")
+        gameOver = true
+        $('#restart').show()
+      }
+    })
+  }
+  newGame()
 
-    turn++
-    // console.log(turn)
-    if (turn % 2 === 0) {
-      currentPlayer = playerTwo
-    } else {
+  const restart = function () {
+    $('#restart').on('submit', function (event) {
+      event.preventDefault()
+      $('#restart').hide()
+      turn = 1
       currentPlayer = playerOne
+      gameBoard[0] = ''
+      gameBoard[1] = ''
+      gameBoard[2] = ''
+      gameBoard[3] = ''
+      gameBoard[4] = ''
+      gameBoard[5] = ''
+      gameBoard[6] = ''
+      gameBoard[7] = ''
+      gameBoard[8] = ''
+      $('#winner-message').text('')
+      $('.spaces').html('')
+      newGame()
     }
-    $(event.target).off('click')
-  })
+    )
+  }
+
+  restart()
 })
