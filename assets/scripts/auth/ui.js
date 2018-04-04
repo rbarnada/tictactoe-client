0a4dc1f4fe7656c2807ab4cbfee87890a5bf3939
rbarnada <ryan.barnada@gmail.com>
2018-04-04 15:50:56 -0400

    Finish style
    finished styling for now

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,55 +2,69 @@ const store = require('../store')
 
 const signUpSuccess = function (data) {
   $('#message').text('Successfully signed up')
-  $('#message').css('background-color', 'green')
+  $('#message').css('background-color', '#7F95A4')
   setTimeout(() => $('#message').text(''), 3000)
+  $('#SignUp').modal('hide')
 }
 
 const signUpFailure = function () {
-  $('#message').text('Something went wrong with signup')
-  setTimeout(() => $('#message').text(''), 3000)
+  $('#up-fail-message').text('Something went wrong with sign up')
+  $('#up-fail-message').css('background-color', 'pink')
+  setTimeout(() => $('#up-fail-message').text(''), 3000)
 }
 
 const signInSuccess = function (data) {
   $('#message').text('Successfully signed in')
-  $('#message').css('background-color', 'green')
+  $('#message').css('background-color', '#7F95A4')
   setTimeout(() => $('#message').text(''), 3000)
-  $('#change-password').removeClass('hidden')
-  $('#sign-out').removeClass('hidden')
-  $('#sign-up').addClass('hidden')
-  $('#sign-in').addClass('hidden')
+  // $('#change-password').removeClass('hidden')
+  // $('#sign-out').removeClass('hidden')
+  // $('#sign-up').addClass('hidden')
+  // $('#sign-in').addClass('hidden')
+  $('#SignIn').modal('hide')
   $('#new-game').removeClass('hidden')
   $('#games-played').removeClass('hidden')
+  // $('#SignUp').modal('hide')
+  $('#sign-in-modal').addClass('hidden')
+  $('#sign-up-modal').addClass('hidden')
+  $('#pass-modal').removeClass('hidden')
+  $('#sign-out').removeClass('hidden')
   store.user = data.user
   // console.log(data.user.id)
 }
 
 const signInFailure = function () {
-  $('#message').text('Could not sign in')
-  $('#message').css('background-color', 'red')
-  setTimeout(() => $('#message').text(''), 3000)
+  $('#in-fail-message').text('Incorrect Login. Try Again')
+  $('#in-fail-message').css('background-color', 'pink')
+  setTimeout(() => $('#in-fail-message').text(''), 3000)
 }
 
 const changePasswordSuccess = function (data) {
   $('#message').text('Password changed')
-  $('#message').css('background-color', 'green')
+  $('#message').css('background-color', '#7F95A4')
   setTimeout(() => $('#message').text(''), 3000)
+  $('#ChangePass').modal('hide')
 }
 
 const changePasswordFailure = function () {
-  $('#message').text('Could not change password')
-  $('#message').css('background-color', 'red')
-  setTimeout(() => $('#message').text(''), 3000)
+  $('#pass-fail-message').text('Could not change password')
+  $('#pass-fail-message').css('background-color', 'pink')
+  setTimeout(() => $('#pass-fail-message').text(''), 3000)
+  $('#pass-modal').removeClass('hidden')
 }
 
 const signOutSuccess = function (data) {
   $('#message').text('Successfully signed out')
-  $('#message').css('background-color', 'green')
+  $('#message').css('background-color', '#7F95A4')
   setTimeout(() => $('#message').text(''), 3000)
-  $('#change-password').addClass('hidden')
+  // $('#change-password').addClass('hidden')
+  // $('#sign-out').addClass('hidden')
+  // $('#sign-up').removeClass('hidden')
+  // $('#sign-in').removeClass('hidden')
+  $('#sign-in-modal').removeClass('hidden')
+  $('#sign-up-modal').removeClass('hidden')
+  $('#pass-modal').addClass('hidden')
   $('#sign-out').addClass('hidden')
-  $('#sign-up').removeClass('hidden')
-  $('#sign-in').removeClass('hidden')
   $('#new-game').addClass('hidden')
   $('#games-played').addClass('hidden')
   $('.game-board').addClass('hidden')
@@ -60,7 +74,7 @@ const signOutSuccess = function (data) {
 
 const signOutFailure = function () {
   $('#message').text('Something went wrong with sign out')
-  $('#message').css('background-color', 'red')
+  $('#message').css('background-color', 'pink')
   setTimeout(() => $('#message').text(''), 3000)
 }
 module.exports = {
--- assets/scripts/game/gameLogic.js
+++ assets/scripts/game/gameLogic.js
@@ -12,10 +12,9 @@ const runGame = function () {
   // let gameOver = false
   store.gameState = false
   // console.log(gameBoard)
-  // ['', '', '', '', '', '', '', '', '']
   // turn variable. odd numbers = p1, even p2.
   let turn = 1
-  // let gameOver = false
+  // console.log(turn)
 
   const placeMove = function () {
     $('.spaces').on('click', function (event) {
@@ -23,21 +22,16 @@ const runGame = function () {
         $(event.target).append(`<h1>${currentPlayer}</h1>`)
         // console.log($(event.target).text())
         const currentVal = $(event.target).data().value
-        gameBoard[currentVal] = currentPlayer
+        store.game.cells[currentVal] = currentPlayer
+        // turn incrementer
+        turn++
+        // console.log(turn)
       } else {
         $('#message').text('Invalid move. Try again')
-        $('#message').css('background-color', 'red')
+        $('#message').css('background-color', 'pink')
         setTimeout(() => $('#message').text(''), 2000)
-        // $(event.target).off('click')
         // console.log('error')
       }
-
-      // Sets X or O to position in array
-
-
-      // turn incrementer
-      turn += 1
-      // console.log(turn)
 
       // determine player
       if (turn % 2 === 0) {
@@ -47,14 +41,6 @@ const runGame = function () {
         currentPlayer = playerOne
         store.player = currentPlayer
       }
-
-      // $(event.target).off('click')
-
-      // console.log(turn)
-      // console.log(gameBoard)
-
-      // let gameOver = false
-      // store.gameState = gameOver
 
       // check for winner
       if ((gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') ||
@@ -68,7 +54,7 @@ const runGame = function () {
         // gameOver = true
         // console.log(gameOver)
         // console.log('Player 1 wins')
-        $('#user-message').text('Player 1 wins')
+        $('#user-message').text('X wins')
         $('.spaces').off('click')
         store.gameState = true
         $('#new-game').removeClass('hidden')
@@ -82,7 +68,7 @@ const runGame = function () {
         (gameBoard[1] === 'O' && gameBoard[4] === 'O' && gameBoard[7] === 'O') ||
         (gameBoard[2] === 'O' && gameBoard[4] === 'O' && gameBoard[6] === 'O')) {
         // console.log('Player 2 wins')
-        $('#user-message').text('Player 2 wins')
+        $('#user-message').text('O wins')
         $('.spaces').off('click')
         // gameOver = true
         store.gameState = true
@@ -99,83 +85,7 @@ const runGame = function () {
       // console.log(store.game.cells)
     })
   }
-
-  const newGame = function () {
-    placeMove()
-    // $('.spaces').on('click', function (event) {
-    //   $(event.target).append(`<h1>${currentPlayer}</h1>`)
-    //   // Sets X or O to position in array
-    //   const currentVal = $(event.target).data().value
-    //   gameBoard[currentVal] = currentPlayer
-    //   turn += 1
-    //   // console.log(turn)
-    //   if (turn % 2 === 0) {
-    //     currentPlayer = playerTwo
-    //   } else {
-    //     currentPlayer = playerOne
-    //   }
-    //   $(event.target).off('click')
-    //
-    //   // console.log(turn)
-    //   // console.log(gameBoard)
-    //
-    //   // check for winner
-    //   if ((gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') ||
-    //     (gameBoard[0] === 'X' && gameBoard[3] === 'X' && gameBoard[6] === 'X') ||
-    //     (gameBoard[0] === 'X' && gameBoard[4] === 'X' && gameBoard[8] === 'X') ||
-    //     (gameBoard[2] === 'X' && gameBoard[5] === 'X' && gameBoard[8] === 'X') ||
-    //     (gameBoard[6] === 'X' && gameBoard[7] === 'X' && gameBoard[8] === 'X') ||
-    //     (gameBoard[3] === 'X' && gameBoard[4] === 'X' && gameBoard[5] === 'X') ||
-    //     (gameBoard[1] === 'X' && gameBoard[4] === 'X' && gameBoard[7] === 'X') ||
-    //     (gameBoard[2] === 'X' && gameBoard[4] === 'X' && gameBoard[6] === 'X')) {
-    //     console.log('Player 1 wins')
-    //     $('#user-message').text('Player 1 wins')
-    //     $('.spaces').off('click')
-    //     gameOver = true
-    //     // $('#restart').removeClass('hidden')
-    //   } else if ((gameBoard[0] === 'O' && gameBoard[1] === 'O' && gameBoard[2] === 'O') ||
-    //     (gameBoard[0] === 'O' && gameBoard[3] === 'O' && gameBoard[6] === 'O') ||
-    //     (gameBoard[0] === 'O' && gameBoard[4] === 'O' && gameBoard[8] === 'O') ||
-    //     (gameBoard[2] === 'O' && gameBoard[5] === 'O' && gameBoard[8] === 'O') ||
-    //     (gameBoard[6] === 'O' && gameBoard[7] === 'O' && gameBoard[8] === 'O') ||
-    //     (gameBoard[3] === 'O' && gameBoard[4] === 'O' && gameBoard[5] === 'O') ||
-    //     (gameBoard[1] === 'O' && gameBoard[4] === 'O' && gameBoard[7] === 'O') ||
-    //     (gameBoard[2] === 'O' && gameBoard[4] === 'O' && gameBoard[6] === 'O')) {
-    //     console.log('Player 2 wins')
-    //     $('#user-message').text('Player 2 wins')
-    //     $('.spaces').off('click')
-    //     gameOver = true
-    //     // $('#restart').removeClass('hidden')
-    //   } else if (gameBoard[0] !== '' && gameBoard[1] !== '' && gameBoard[2] !== '' && gameBoard[3] !== '' && gameBoard[4] !== '' && gameBoard[5] !== '' && gameBoard[6] !== '' && gameBoard[7] !== '' && gameBoard[8] !== '') {
-    //     $('#user-message').text("It's a tie")
-    //     gameOver = true
-    //     // $('#restart').removeClass('hidden')
-    //   }
-    // })
-  }
-  newGame()
-
-  // const restart = function () {
-  //   $('#restart').on('submit', function (event) {
-  //     event.preventDefault()
-  //     turn = 1
-  //     currentPlayer = playerOne
-  //     // gameBoard[0] = ''
-  //     // gameBoard[1] = ''
-  //     // gameBoard[2] = ''
-  //     // gameBoard[3] = ''
-  //     // gameBoard[4] = ''
-  //     // gameBoard[5] = ''
-  //     // gameBoard[6] = ''
-  //     // gameBoard[7] = ''
-  //     // gameBoard[8] = ''
-  //     $('#user-message').text('')
-  //     $('.spaces').html('')
-  //     newGame()
-  //   })
-  // }
-  //
-  // restart()
+  placeMove()
 }
 
 module.exports = {
